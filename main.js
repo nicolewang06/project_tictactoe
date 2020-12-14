749b8fe20ef31d32282bed6e6b42652dd89897f5
nicolewang06 <73081848+nicolewang06@users.noreply.github.com>
2020-12-14 18:00:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let secondPlayer;
 
 const winningMoves = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ];
 const squares = Array.from(document.querySelectorAll('#board div'));
-const statusMsg = document.getElementById('intro');
+const statusMsg = document.getElementById('status');
 
 document.getElementById('board').addEventListener('click', handleTurn);
 document.getElementById('restart').addEventListener('click', start);
