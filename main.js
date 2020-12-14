3c1de5a8b6196f2b1ed3ddb815dc5ab7e865d178
nicolewang06 <73081848+nicolewang06@users.noreply.github.com>
2020-12-14 17:15:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ let compPlayer = 'O';
 let win;
 let xScore = 0;
 let oScore = 0;
+let firstPlayer;
+let secondPlayer;
+
 
 const winningMoves = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ];
 const squares = Array.from(document.querySelectorAll('#board div'));
@@ -53,15 +56,14 @@ function gamePlay() {
     });
 
     statusMsg.textContent = win === 'tie' ? `it's a TIE!` : win ? `${win} is the winner !` : `It's ${currentPlayer}'s turn!`;
-    
     };
 
 function play () {
     event.preventDefault();
     let firstPlayer = document.getElementById("player1").value;
     let secondPlayer = document.getElementById("player2").value;    
-    document.getElementById("firstPlayer").innerText = firstPlayer+":";
-    document.getElementById("secondPlayer").innerText = secondPlayer+":";
+    document.getElementById("firstPlayer").innerText = "X is " + firstPlayer;
+    document.getElementById("secondPlayer").innerText = "O is " + secondPlayer;
 }
 
 start();
