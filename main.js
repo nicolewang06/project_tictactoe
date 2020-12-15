bc5be9d82bc046dc9a13d2c53b09ec28bdb9d44d
nicolewang06 <73081848+nicolewang06@users.noreply.github.com>
2020-12-15 16:27:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let oScore = 0;
 let firstPlayer;
 let secondPlayer;
 let active = 1;
+let theWinner;
 
 const winningMoves = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ];
 const squares = Array.from(document.querySelectorAll('#board div'));
@@ -68,7 +69,14 @@ function gameOver() {
     //for (var i = 0; i < squares.length; i++) {
         active = 0;
         document.getElementById('board').removeEventListener('click', handleTurn, false);
-        alert(`${win} is the winner. play again ?`);
+        let firstPlayer = document.getElementById("player1").value;
+        let secondPlayer = document.getElementById("player2").value;
+        if (win === "X")   {
+            theWinner = firstPlayer
+        }else {
+            theWinner = secondPlayer
+        }
+        alert(`${theWinner} is the winner. play again ?`);
         start();
     }
 
